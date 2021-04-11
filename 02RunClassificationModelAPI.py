15c7ca627d37f9179b8d3778382e9df71722687c
viniciusmsousa <vinisousa04@gmail.com>
2021-04-11 11:21:00 -0300

    fix: Serve both UI and Model API in same script

--- 02RunClassificationModelAPI.py
+++ 02RunClassificationModelAPI.py
@@ -1,4 +1,5 @@
-from subprocess import Popen 
+import os
+
 
 from loguru import logger
 logger.add('logs/logs.log', rotation = '5 MB')
@@ -6,17 +7,13 @@ import pandas as pd
 import mlflow
 from settings import EXPERIMENT_NAME, CLASSIFICATION_SORT_METRIC_H2O, MLFLOW_CONDA_HOME
 
-logger.info('Start MLFLOW UI')
-ui = Popen('mlflow ui -p 5000', shell = True)
-ui.wait()
-
+# logger.info('Start MLFLOW UI')
+# ui = Popen('mlflow ui -p 5000', shell = True)
+# ui.wait()
 
 logger.info(f'Getting Best Run from Experiment {EXPERIMENT_NAME}, sorted by {CLASSIFICATION_SORT_METRIC_H2O}')
 best_model_id = mlflow.search_runs(experiment_ids=[str(i) for i in range(1,3)], order_by=['metrics.aucpr DESC'])['run_id'][0]
 
 logger.info('Startting API')
-api = Popen([
-    f'export MLFLOW_CONDA_HOME="{MLFLOW_CONDA_HOME}"', '&&' ,
-    'mlflow models serve -m runs:/', best_model_id, '/model -p 5001'
-], shell = True)
-api.wait()
+os.system(f'export MLFLOW_CONDA_HOME="{MLFLOW_CONDA_HOME}"')
+os.system(f'mlflow models serve -m runs:/{best_model_id}/model -p 5001')
