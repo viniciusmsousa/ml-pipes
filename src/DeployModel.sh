2cff851b364724332aebc3647b65eb513b603af4
Vini <vinisousa04@gmail.com>
2021-04-13 19:35:52 -0300

    fix: Model name in DeployModel.sh

--- src/DeployModel.sh
+++ src/DeployModel.sh
@@ -3,8 +3,6 @@
 # Kill process
 #lsof -i :5000
 
-echo "Deploying Production model name=CreditCardDefault"
-
-
+echo "Deploying Production model name=TestCreditCard"
 export MLFLOW_TRACKING_URI=sqlite:///mlruns.db
-mlflow models serve -p 5001 --model-uri models:/CreditCardDefault/staging --no-conda
+mlflow models serve -p 5001 --model-uri models:/TestCreditCard/production --no-conda
