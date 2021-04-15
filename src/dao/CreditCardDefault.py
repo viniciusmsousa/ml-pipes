b2745f144a537db54034285a7c7b19ff7fbf1365
Vini <vinisousa04@gmail.com>
2021-04-14 22:47:38 -0300

    refactor: Load Test Credit Card Dataset

--- src/dao/CreditCardDefault.py
+++ src/dao/CreditCardDefault.py
@@ -5,7 +5,10 @@ from settings import PATH_CREDIT_CARD_DATASET
 
 def load_creditcard_dataset():
     try:
-        return pd.read_csv(PATH_CREDIT_CARD_DATASET)
+        df = pd.read_csv(PATH_CREDIT_CARD_DATASET)
+        df_nonevent = df.loc[df['Class']==0].sample(1500)
+        df_event = df.loc[df['Class']==1].sample(250)
+        return df_nonevent.append(df_event)
     except Exception as e:
         logger.error(e)
         raise Exception(e)
