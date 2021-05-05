641d8c4d0c895b978985224e51ec107a360e49ac
Vini <vinisousa04@gmail.com>
2021-05-05 18:21:16 -0300

    refactor: add MLFlow TRACKING_URI to settings file

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,7 @@
 # Experiment Context
 EXPERIMENT_NAME = "CreditCardDefault"
 PATH_CREDIT_CARD_DATASET = "dao/data/creditcard.csv"
+TRACKING_URI = 'sqlite:///mlruns.db'
 
 # Model Contex
 THRESHOLD = 0.5
