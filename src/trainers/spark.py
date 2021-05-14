3aec22011e60ed793e3ca4bf2659babac204ff6c
Vini <vinisousa04@gmail.com>
2021-05-14 17:58:29 -0300

    style: spark classifiers

--- src/trainers/spark.py
+++ src/trainers/spark.py
@@ -5,7 +5,8 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
 from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.classification import LogisticRegression, DecisionTreeClassifier, RandomForestClassifier, GBTClassifier
+from pyspark.ml.classification import LogisticRegression, \
+    DecisionTreeClassifier, RandomForestClassifier, GBTClassifier
 
 import mlflow
 import mlflow.spark
@@ -14,43 +15,62 @@ from ds_toolbox.statistics import ks_test
 
 
 @typechecked
-def classification_metrics(dfs_prediction: pyspark.sql.dataframe.DataFrame, col_target: str, print_metrics: bool = False):
+def classification_metrics(
+    dfs_prediction: pyspark.sql.dataframe.DataFrame,
+    col_target: str,
+    print_metrics: bool = False
+):
     """Function to compute a few binary classification metrics from a spark df.
 
     Args:
-        dfs_prediction (pyspark.sql.dataframe.DataFrame): SparkDataFrame with label and prediction columns.
+        dfs_prediction (pyspark.sql.dataframe.DataFrame): SparkDataFrame with
+        label and prediction columns.
         col_target (str): Label colum name.
-        print_metrics (bool, optional): Whether or not to print the metrics. Defaults to False.
+        print_metrics (bool, optional): Whether or not to print the metrics.
+        Defaults to False.
 
     Raises:
-        Exception: Erros.
+        Exception: Errors.
 
     Returns:
         dict: Dict with metrics.
     """
     try:
         # Confusion Matrix
-        confusion_matrix = dfs_prediction.groupBy(col_target, "prediction").count()
-        TN = dfs_prediction.filter(f'prediction = 0 AND {col_target} = 0').count()
-        TP = dfs_prediction.filter(f'prediction = 1 AND {col_target} = 1').count()
-        FN = dfs_prediction.filter(f'prediction = 0 AND {col_target} = 1').count()
-        FP = dfs_prediction.filter(f'prediction = 1 AND {col_target} = 0').count()
+        confusion_matrix = dfs_prediction\
+            .groupBy(col_target, "prediction").count()
+        TN = dfs_prediction\
+            .filter(f'prediction = 0 AND {col_target} = 0').count()
+        TP = dfs_prediction\
+            .filter(f'prediction = 1 AND {col_target} = 1').count()
+        FN = dfs_prediction\
+            .filter(f'prediction = 0 AND {col_target} = 1').count()
+        FP = dfs_prediction\
+            .filter(f'prediction = 1 AND {col_target} = 0').count()
 
         # Computing Metrics from Confusion Matrix
         accuracy = (TN + TP) / (TN + TP + FN + FP)
         precision = TP/(TP+FP) if (TP+FP) > 0 else 0
         recall = TP/(TP+FN)
-        f1 = 2*(precision*recall/(precision+recall)) if (precision+recall) > 0 else 0
+        f1 = 2*(precision*recall/(precision+recall)) if (precision+recall) > 0 else 0  # noqa
 
         # PySpark (2.3.4) Evaluation Metrics
         evaluator = BinaryClassificationEvaluator(labelCol=col_target)
         auroc = evaluator.evaluate(dfs_prediction)
 
         # digio_ds_toolkit lib
-        df_predictions = dfs_prediction.rdd.map(lambda row: (row.prediction, ) + tuple(row.probability.toArray().tolist())).toDF(["prediction"])
-        df_predictions = df_predictions.withColumnRenamed('_3','prob_1')
-        df_predictions = df_predictions.select('prediction', 'prob_1').toPandas()
-        ks_dict = ks_test(df=df_predictions, col_target='prediction', col_probability='prob_1')
+        df_predictions = dfs_prediction.rdd.map(
+            lambda row: (row.prediction, ) +
+                tuple(row.probability.toArray().tolist())  # noqa
+        ).toDF(["prediction"])
+        df_predictions = df_predictions.withColumnRenamed('_3', 'prob_1')
+        df_predictions = df_predictions\
+            .select('prediction', 'prob_1').toPandas()  # type: ignore
+        ks_dict = ks_test(
+            df=df_predictions,
+            col_target='prediction',
+            col_probability='prob_1'
+        )
 
         # Results Dict
         out_dict = {
@@ -62,7 +82,7 @@ def classification_metrics(dfs_prediction: pyspark.sql.dataframe.DataFrame, col_
             'auroc': auroc,
             'ks': ks_dict
         }
-        
+
         if print_metrics is True:
             print(f'accuracy:  {round(out_dict["accuracy"], 4)}')
             print(f'precision: {round(out_dict["precision"], 4)}')
@@ -70,17 +90,19 @@ def classification_metrics(dfs_prediction: pyspark.sql.dataframe.DataFrame, col_
             print(f'f1:        {round(out_dict["f1"], 4)}')
             print(f'auroc:     {round(out_dict["auroc"], 4)}')
             print(f'max_ks:    {round(out_dict["ks"]["max_ks"], 4)}')
-        
+
         return out_dict
     except Exception as e:
         raise Exception(e)
+
 
 @typechecked
 def init_spark_session(max_mem_size: int = 4, n_cores: int = 4):
     """Initialize a local Spark Session.
 
     Args:
-        max_mem_size (int, optional): Max Memory in GB to be allocated to Spark. Defaults to 4.
+        max_mem_size (int, optional): Max Memory in GB to be allocated
+        to Spark. Defaults to 4.
         n_cores (int, optional): Cores to be allocated to Spark. Defaults to 4.
 
     Raises:
@@ -97,12 +119,13 @@ def init_spark_session(max_mem_size: int = 4, n_cores: int = 4):
             .master(f'local[{n_cores}]') \
             .config('spark.executor.memory', mem_size) \
             .config('spark.driver.memory', mem_size) \
-            .config('spark.memory.offHeap.enabled','true') \
-            .config('spark.memory.offHeap.size',mem_size) \
+            .config('spark.memory.offHeap.enabled', 'true') \
+            .config('spark.memory.offHeap.size', mem_size) \
             .getOrCreate()
         return spark
     except Exception as e:
         raise Exception(e)
+
 
 class SparkClassifier:
     """
@@ -118,14 +141,16 @@ class SparkClassifier:
         n_cores: int = 4,
         seed: int = 90
     ):
-        """Wrapper fits DecisionTreeClassifier, 
+        """Wrapper fits DecisionTreeClassifier,
             RandomForestClassifier, LogisticRegression and GBTClassifier.
 
         Args:
             df (pd.DataFrame): Dataset with target and feature.
             target_col (str): Target column to be predicted.
-            run_name (str, optional): MLFlow run name. Defaults to 'spark_classifier'.
-            max_mem_size (str, optional): Max memory to the allocated to Spark. Defaults to '4G'.
+            run_name (str, optional): MLFlow run name.
+                Defaults to 'spark_classifier'.
+            max_mem_size (str, optional): Max memory to the allocated to Spark.
+                Defaults to '4G'.
             n_cores (str, optional): Number of cores to be used. Defaults to 4.
             seed (int, optional): Seed. Defaults to 90.
         """
@@ -138,61 +163,79 @@ class SparkClassifier:
         self.seed = seed
 
         # 1) Starting Spark Session
-        self.spark = init_spark_session(n_cores=self.n_cores, max_mem_size=self.max_mem_size)
-        
+        self.spark = init_spark_session(
+            n_cores=self.n_cores, max_mem_size=self.max_mem_size
+        )
+
         # 2) Getting Features Cols
         self.feature_cols = self.get_feature_cols()
-        
+
         # 3) Getting spark dataframe
         self.dfs_train, self.dfs_test = self.transform_spark()
 
-        self.classfiers = self.classfiers()
+        self.classfiers = self.classfiers()  # type: ignore
 
         self.shutdown_spark()
 
     def get_feature_cols(self):
         try:
-           return list(self.df.columns[self.df.columns != self.target_col])
+           return list(self.df.columns[self.df.columns != self.target_col])  # noqa
         except Exception as e:
             raise Exception(e)
 
     def transform_spark(self):
         try:
             dfs = self.spark.createDataFrame(self.df)
-            assembler = VectorAssembler(inputCols=self.feature_cols, outputCol='features')
+            assembler = VectorAssembler(
+                inputCols=self.feature_cols, outputCol='features'
+            )
             dfs = assembler.transform(dfs)
             dfs_train, dfs_test = dfs.randomSplit([0.8, 0.2], seed=self.seed)
             return dfs_train, dfs_test
         except Exception as e:
             raise Exception(e)
 
-
     def classfiers(self):
         try:
-            lr = LogisticRegression(labelCol=self.target_col, featuresCol='features')
+            lr = LogisticRegression(
+                labelCol=self.target_col, featuresCol='features'
+            )
             lr_model = lr.fit(self.dfs_train)
-            
-            dt = DecisionTreeClassifier(labelCol=self.target_col, featuresCol='features')
+
+            dt = DecisionTreeClassifier(
+                labelCol=self.target_col, featuresCol='features'
+            )
             dt_model = dt.fit(self.dfs_train)
 
-            rf = RandomForestClassifier(labelCol=self.target_col, featuresCol='features')
+            rf = RandomForestClassifier(
+                labelCol=self.target_col, featuresCol='features'
+            )
             rf_model = rf.fit(self.dfs_train)
 
-            gbt = GBTClassifier(labelCol=self.target_col, featuresCol='features')
+            gbt = GBTClassifier(
+                labelCol=self.target_col, featuresCol='features'
+            )
             gbt_model = gbt.fit(self.dfs_train)
 
-            names = ['LogisticRegression', 'DecisionTreeClassifier', 'RandomForestClassifier', 'GBTClassifier']
+            names = [
+                'LogisticRegression', 'DecisionTreeClassifier',
+                'RandomForestClassifier', 'GBTClassifier'
+            ]
             models = [lr_model, dt_model, rf_model, gbt_model]
             for name, model in zip(names, models):
                 prediction = model.transform(self.dfs_test)
-                metrics = classification_metrics(dfs_prediction=prediction, col_target=self.target_col, print_metrics=False)
+                metrics = classification_metrics(
+                    dfs_prediction=prediction,
+                    col_target=self.target_col,
+                    print_metrics=False
+                )
 
-                with mlflow.start_run(run_name=f'{self.run_name}_{name}') as run:
+                with mlflow.start_run(run_name=f'{self.run_name}_{name}'):
                     mlflow.log_metric("f1", metrics["f1"])
                     mlflow.log_metric("auc", metrics["auroc"])
                     mlflow.log_metric('accuracy', metrics["accuracy"])
                     mlflow.log_metric("precision", metrics["precision"])
-                    mlflow.log_metric("recall", metrics["recall"]) 
+                    mlflow.log_metric("recall", metrics["recall"])
                     mlflow.log_metric("ks", metrics["ks"]["max_ks"])
 
                     mlflow.spark.log_model(model, "model")
